165905ef6ffbda9a1901a9c43dd7e868a4887727
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2024-12-12 12:29:50 +0200

    chore: bump contracts to use upgradeable factory

--- packages/auth-server/stores/client.ts
+++ packages/auth-server/stores/client.ts
@@ -29,8 +29,8 @@ export const contractsByChain: Record<SupportedChainId, ChainContracts> = {
   [zksyncInMemoryNode.id]: {
     session: "0x62b019c03Db1f4d0C0b857fDabE4AbC9c60c2F70",
     passkey: "0x21b8397BeF5128662564b8491676baa6754AFD47",
-    accountFactory: "0xd7385ba726A7b72933E63FCb0Dfee8Bcae63478c",
-    accountPaymaster: "0xAF34a25b78066f2442fb4d7cA740A03E47769b71",
+    accountFactory: "0x26711A4A572a5BBdF967b6385636Bd968e6E883C",
+    accountPaymaster: "0xB29C8Dd9a5E5DFeFB49Adf16885175a07DE8e541",
   },
 };
 
