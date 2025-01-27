065e764d7f9e2baaeb0fbd6ecc76c7504389145a
aon <21188659+aon@users.noreply.github.com>
2025-01-27 11:35:35 -0300

    feat: add integration with /recovery/guardian/find-account

--- packages/auth-server/composables/useRecoveryGuardian.ts
+++ packages/auth-server/composables/useRecoveryGuardian.ts
@@ -1,1 +1,35 @@
-export const useRecoveryGuardian = () => {};
+import type { Address } from "viem";
+import { GuardianRecoveryModuleAbi } from "zksync-sso/abi";
+
+export const useRecoveryGuardian = () => {
+  const { getPublicClient, defaultChain } = useClientStore();
+
+  const getGuardedAccountsInProgress = ref(false);
+  const getGuardedAccountsError = ref<Error | null>(null);
+
+  async function getGuardedAccounts(guardianAddress: Address) {
+    getGuardedAccountsInProgress.value = true;
+    getGuardedAccountsError.value = null;
+
+    try {
+      const client = getPublicClient({ chainId: defaultChain.id });
+      return await client.readContract({
+        address: contractsByChain[defaultChain.id].recovery,
+        abi: GuardianRecoveryModuleAbi,
+        functionName: "guardianOf",
+        args: [guardianAddress],
+      });
+    } catch (err) {
+      getGuardedAccountsError.value = err as Error;
+      return [];
+    } finally {
+      getGuardedAccountsInProgress.value = false;
+    }
+  }
+
+  return {
+    getGuardedAccountsInProgress,
+    getGuardedAccountsError,
+    getGuardedAccounts,
+  };
+};
