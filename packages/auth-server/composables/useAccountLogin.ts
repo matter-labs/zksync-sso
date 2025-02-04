3bcbaf414d4cc14fff394f572f17d1e92c28ec2d
aon <21188659+aon@users.noreply.github.com>
2025-02-04 11:19:31 -0300

    feat: add verify recovery view on the main page

--- packages/auth-server/composables/useAccountLogin.ts
+++ packages/auth-server/composables/useAccountLogin.ts
@@ -8,16 +8,46 @@ export const useAccountLogin = (_chainId: MaybeRef<SupportedChainId>) => {
 
   const { inProgress: loginInProgress, error: accountLoginError, execute: loginToAccount } = useAsync(async () => {
     const client = getPublicClient({ chainId: chainId.value });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const { username, address, passkeyPublicKey } = await fetchAccount(client as any, {
-      contracts: contractsByChain[chainId.value],
-    });
 
-    login({
-      username,
-      address,
-      passkey: toHex(passkeyPublicKey),
-    });
+    const credential = await navigator.credentials.get({
+      publicKey: {
+        challenge: new Uint8Array(32),
+        userVerification: "discouraged",
+      },
+    }) as PublicKeyCredential | null;
+    if (!credential) {
+      throw new Error("No credential found");
+    }
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const { username, address, passkeyPublicKey } = await fetchAccount(client as any, {
+        contracts: contractsByChain[chainId.value],
+        uniqueAccountId: credential.id,
+      });
+
+      login({
+        username,
+        address,
+        passkey: toHex(passkeyPublicKey),
+      });
+      return { success: true } as const;
+    } catch {
+      const { checkRecoveryRequest } = useRecoveryGuardian();
+      const recoveryRequest = await checkRecoveryRequest(credential.id);
+      if (recoveryRequest) {
+        return {
+          success: false,
+          recoveryRequest: {
+            account: recoveryRequest[0],
+            isReady: recoveryRequest[1],
+            remainingTime: recoveryRequest[2],
+          },
+        } as const;
+      } else {
+        return { success: false } as const;
+      }
+    }
   });
 
   return {
