0731e8aeb34f408a5789e6e1ed89c0e2d2c7fa26
cbe <cbe@matterlabs.dev>
2025-02-11 10:16:36 -0800

    fix: remove debugging changes
    You just have to know that the quest address is added via .env.local
    so the object in the config isn't used

--- packages/auth-server/components/nft/zeek-nft-quest.vue
+++ packages/auth-server/components/nft/zeek-nft-quest.vue
@@ -58,7 +58,6 @@ const getNFTTransactions = async function () {
   const { getPublicClient, defaultChain } = useClientStore();
 
   const client = getPublicClient({ chainId: chainId ?? defaultChain.id });
-  console.log("nftAddress", nftAddress, "ZeekNftQuestAbi", ZeekNftQuestAbi, "address", address);
   const res = await client.readContract({
     address: nftAddress,
     abi: ZeekNftQuestAbi,
