f068a29d11aae2c39ebbd257dfa2daedef511da9
Matías Ignacio González <maigonzalez@fi.uba.ar>
2025-02-13 11:29:42 -0300

    feat: Add oidc key registry server (#59)
    * Add oidc key registry server
    
    * Remove validator
    
    * Update getIssHash

---
+++ packages/oidc-key-registry-server/src/config.ts
@@ -0,0 +1,20 @@
+import { createEnv } from "@t3-oss/env-core";
+import { types } from "zksync-ethers";
+import { z } from "zod";
+
+const validNetworks = ["mainnet", "sepolia", "localhost"] as const;
+
+export const config = createEnv({
+  server: {
+    FETCH_INTERVAL: z.preprocess(
+      (val) => (val === undefined ? 60 * 1000 : Number(val)),
+      z.number()
+    ),
+    ZKSYNC_PRIVATE_KEY: z.string(),
+    CONTRACT_ADDRESS: z.string(),
+    NETWORK: z.enum(validNetworks).optional(),
+    RPC_URL: z.string().url().optional(),
+  },
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+});
