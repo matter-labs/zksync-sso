95ef3b2d4b006c1804848d7facf9d0834eb2cb0e
Miguel Duarte <miguelfeliped@gmail.com>
2025-02-19 08:42:01 -0300

    OIDC/update sub repos (#70)
    * chore: updated submodules
    
    Updated circuits and contracts submodules.
    Added idea files to .gitignore
    
    * chore: update sub repos
    
    Update circuits and contracts sub modules.
    
    * chore: updated circuit submodule
    
    updated circuit submodule
    
    * chore: fix auth server lint
    
    using automatic eslint fix on authserver.
    
    * chore: lint for key registry
    
    Make key registry package pass lint.
    Updated logic to fetch keys considering that now the contract reverts when a key does not exist.
    
    * chore: apply standards
    
    - Make oidc key registry server use regular node instead of bun.
    - Code adapted to lint rules.
    
    * chore: ci and lint
    
    little adjustments for ci and lint.
    
    * chore: added jose to spelling
    
    * chore: ignoring circuits in cspell
    
    * chore: bump circuits
    
    * chore: bump es build target
    
    * fix: disabled server side rendering

--- packages/oidc-key-registry-server/src/fetchers/google.ts
+++ packages/oidc-key-registry-server/src/fetchers/google.ts
@@ -1,5 +1,11 @@
 import type { Key, KeyFetcher } from "../types";
 
+type JWK = {
+  kid: string;
+  n: string;
+  e: string;
+};
+
 export class GoogleFetcher implements KeyFetcher {
   private apiUrl = "https://www.googleapis.com/oauth2/v3/certs";
 
@@ -8,7 +14,7 @@ export class GoogleFetcher implements KeyFetcher {
     if (!response.ok) throw new Error(`Google API error: ${response.status}`);
 
     const data = await response.json();
-    return data.keys.map((key: any) => ({
+    return data.keys.map((key: JWK) => ({
       kid: this.toBytes32(key.kid),
       n: this.toHex(key.n),
       e: this.toHex(key.e),
@@ -16,10 +22,10 @@ export class GoogleFetcher implements KeyFetcher {
   }
 
   toBytes32(str: string): string {
-    return `0x${str.padStart(64, '0')}`;
+    return `0x${str.padStart(64, "0")}`;
   }
 
   toHex(str: string): string {
-    return `0x${Buffer.from(str, 'base64url').toString('hex')}`;
+    return `0x${Buffer.from(str, "base64url").toString("hex")}`;
   }
 }
