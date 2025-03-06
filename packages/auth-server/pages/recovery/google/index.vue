abe4c309d71545c12282333d788f598677904154
calvo.generico <miguelfeliped@gmail.com>
2025-03-06 12:40:45 -0300

    test: logging info needed to check data

--- packages/auth-server/pages/recovery/google/index.vue
+++ packages/auth-server/pages/recovery/google/index.vue
@@ -141,6 +141,7 @@ async function generateProf(): Promise<void> {
 
   guide("Generating proof...");
   const res = await snarkjs.groth16.fullProve(inputs.toObject(), "/circuit/witness.wasm", "/circuit/circuit.zkey", console);
+  console.log(res);
 
   guide("Adding proof...");
   oidcClient.account.addProof({
