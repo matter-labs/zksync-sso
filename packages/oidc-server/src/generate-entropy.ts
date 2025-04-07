0b6d4e94372db078de65a82938f818597033397b
calvo.generico <miguelfeliped@gmail.com>
2025-04-07 08:54:23 -0300

    feat: improve entropy script

--- packages/oidc-server/src/generate-entropy.ts
+++ packages/oidc-server/src/generate-entropy.ts
@@ -4,31 +4,52 @@ import { randomBytes } from "node:crypto";
 import { readFile, writeFile } from "node:fs/promises";
 import * as path from "node:path";
 
+import { bytesToHex } from "viem";
+
 const dirName = import.meta.dirname;
 
-async function main() {
-  const provided = process.env.SALT_ENTROPY;
-  if (provided === undefined || provided === "") {
-    const newEntropy = randomBytes(48);
-    const dotEnvPath = path.join(dirName, "..", ".env");
-    const dotEnvFile = await readFile(dotEnvPath, "utf8")
-      .catch(() => "");
-    const filtered = dotEnvFile.replace(/.*SALT_ENTROPY=.*/g, "");
+function generateNewEntropy(): string {
+  return bytesToHex(randomBytes(48));
+}
 
-    await writeFile(dotEnvPath, `${filtered}\nSALT_ENTROPY="0x${newEntropy.toString("hex")}"\n`);
-    console.log(`SALT ENTROPY: 0x${newEntropy.toString("hex")}`);
+function noEntropyProvided(): boolean {
+  const provided = process.env.SALT_ENTROPY;
+  return provided === undefined || provided === "";
+}
+
+async function readDotEnv(): Promise<string> {
+  const dotEnvPath = path.join(dirName, "..", ".env");
+  return readFile(dotEnvPath, "utf8")
+    .catch(() => "").then((s) => s.trim());
+}
+
+async function writeDotEnv(content: string): Promise<void> {
+  const dotEnvPath = path.join(dirName, "..", ".env");
+  await writeFile(dotEnvPath, content);
+}
+
+async function main() {
+  if (noEntropyProvided()) {
+    const newEntropy = generateNewEntropy();
+    const dotEnvFile = await readDotEnv();
+    const filtered = dotEnvFile.replace(/.*SALT_ENTROPY=.*/g, "");
+    await writeDotEnv(`${filtered}\nSALT_ENTROPY="${newEntropy}"\n`);
+    console.log(`SALT ENTROPY: ${newEntropy}`);
     process.exit(0);
-  } else {
-    if (/^0x[0-9a-fA-F]+$/.test(provided)) {
-      console.log(`SALT ENTROPY: ${provided}`);
-    } else {
-      console.log(
-        "There is an entropy already set, but it's wrongly generated. Entropy should be encoded as "
-        + "a hex string. Please remove SALT_ENTROPY env variable and run again.",
-      );
-      process.exit(1);
-    }
   }
+
+  const provided = process.env.SALT_ENTROPY!;
+
+  if (/^0x[0-9a-fA-F]+$/.test(provided)) {
+    console.log(`Entropy was already present.\nSALT ENTROPY: ${provided}`);
+    process.exit(0);
+  }
+
+  console.log(
+    "There is an entropy already set, but it's wrongly generated. Entropy should be encoded as "
+    + "a hex string. Please remove SALT_ENTROPY env variable and run again.",
+  );
+  process.exit(1);
 }
 
 await main();
