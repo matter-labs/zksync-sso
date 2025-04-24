70e9ef415cf2878efa73f7e837e200869f1fcc8e
calvo.generico <miguelfeliped@gmail.com>
2025-04-24 18:26:53 -0300

    fix: adapted key fetcher to new abi
    using viem to interact with contracts instead of ethers.

--- packages/oidc-server/src/fetchers/google.ts
+++ packages/oidc-server/src/fetchers/google.ts
@@ -1,5 +1,7 @@
+import { bytesToHex, pad } from "viem";
 import { z } from "zod";
 
+import { GOOGLE_CERTS_URL } from "../constants.js";
 import type { BaseKey, KeyFetcher } from "../types.js";
 
 const jwkSchema = z.object({
@@ -15,25 +17,19 @@ const keyResponseSchema = z.object({
 type JWK = z.infer<typeof jwkSchema>;
 
 export class GoogleFetcher implements KeyFetcher {
-  private apiUrl = "https://www.googleapis.com/oauth2/v3/certs";
-
   async fetchKeys(): Promise<BaseKey[]> {
-    const response = await fetch(this.apiUrl);
+    const response = await fetch(GOOGLE_CERTS_URL);
     if (!response.ok) throw new Error(`Google API error: ${response.status}`);
 
     const data = await response.json().then((keys) => keyResponseSchema.parse(keys));
     return data.keys.map((key: JWK) => ({
       kid: this.toBytes32(key.kid),
       n: key.n,
-      e: this.toHex(key.e),
     }));
   }
 
   private toBytes32(str: string): string {
-    return `0x${str.padStart(64, "0")}`;
-  }
-
-  private toHex(str: string): string {
-    return `0x${Buffer.from(str, "base64url").toString("hex")}`;
+    const bytes = Buffer.from(str, "hex");
+    return bytesToHex(pad(bytes));
   }
 }
--- packages/oidc-server/src/update-keys-service.ts
+++ packages/oidc-server/src/update-keys-service.ts
@@ -1,14 +1,15 @@
+import { GOOGLE_ISS } from "./constants.js";
 import { ContractUpdater } from "./contractUpdater.js";
 import { env } from "./env.js";
 import { GoogleFetcher } from "./fetchers/google.js";
 
 const main = async () => {
   const fetcher = new GoogleFetcher();
-  const contractUpdater = new ContractUpdater();
+  const contractUpdater = new ContractUpdater(env.CONTRACT_ADDRESS, env.RPC_URL, env.ADMIN_PRIVATE_KEY, env.NETWORK);
 
   try {
     const keys = await fetcher.fetchKeys();
-    await contractUpdater.updateContract("https://accounts.google.com", keys);
+    await contractUpdater.updateContract(GOOGLE_ISS, keys);
   } catch (error) {
     console.error("Error fetching keys:", error);
   }
