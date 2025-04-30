3e79bc5db0efe88a7b8f28fba4d650882e2bbb89
Miguel Duarte <miguelfeliped@gmail.com>
2025-04-30 16:42:48 -0300

    OIDC/changes for audit (#122)
    * feat: using all passkey data to generate hash
    
    * feat: using passkeyHash and targetAddress as part jwt nonce content
    
    * fix: adapted key fetcher to new abi
    
    using viem to interact with contracts instead of ethers.
    
    * fix: removed dependencies not being used anymore
    
    * chore: updated OidcKeyRegistry abi in sdk
    
    * chore: using new versions of subpackages
    
    * chore: bump submodules
    
    * chore: bump subrepos and autogenerated files
    
    * feat: added dockerfile for oidc server
    
    * feat: variable rename
    
    * chore: bump contracts submodule
    
    * chore: bump circuits
    
    * chore: bump versions
    
    * chore: bump contracts submodule
    
    * chore: bump pnpm lockfile

--- packages/oidc-server/src/contractUpdater.ts
+++ packages/oidc-server/src/contractUpdater.ts
@@ -1,38 +1,44 @@
-import { Contract, Provider, types, Wallet } from "zksync-ethers";
+import { createWalletClient, type Hex, http, publicActions } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { zksync, zksyncInMemoryNode, zksyncSepoliaTestnet } from "viem/chains";
+import { OidcKeyRegistryAbi } from "zksync-sso/abi";
 import { CircomBigInt } from "zksync-sso-circuits";
 
-import { abi } from "./abi.js";
-import { env } from "./env.js";
-import type { BaseKey, Key } from "./types.js";
+import { type BaseKey, type Key, keySchema, type ValidNetworks } from "./types.js";
 
 export class ContractUpdater {
-  private wallet: Wallet;
-  private provider: Provider;
-  private contract: Contract;
-  private issHashes = new Map<string, string>();
+  private issHashes = new Map<string, Hex>();
+  private rpcUrl: string;
+  private contractAddress: Hex;
+  private privKey: Hex;
+  private network: typeof zksyncInMemoryNode | typeof zksync | typeof zksyncSepoliaTestnet;
 
-  constructor() {
-    if (env.RPC_URL) {
-      this.provider = new Provider(env.RPC_URL);
-    } else if (env.NETWORK) {
-      this.provider = Provider.getDefaultProvider(this.getNetwork());
-    } else {
-      throw new Error("Either RPC_URL or NETWORK must be set");
-    }
-    this.wallet = new Wallet(env.ADMIN_PRIVATE_KEY, this.provider);
+  constructor(contractAddress: Hex, rpcUrl: string, privKey: Hex, networkName: ValidNetworks) {
+    // this.wallet = new Wallet(env.ADMIN_PRIVATE_KEY, this.provider);
+    this.rpcUrl = rpcUrl;
+    this.privKey = privKey;
+    this.contractAddress = contractAddress;
+    this.network = networkName === "local"
+      ? zksyncInMemoryNode
+      : networkName === "sepolia"
+        ? zksyncSepoliaTestnet
+        : zksync;
+  }
 
-    this.contract = new Contract(
-      env.CONTRACT_ADDRESS,
-      abi,
-      this.wallet,
-    );
+  private client() {
+    return createWalletClient({
+      chain: this.network,
+      account: privateKeyToAccount(this.privKey),
+      transport: http(this.rpcUrl),
+    }).extend(publicActions);
   }
 
   public async updateContract(iss: string, keys: BaseKey[]): Promise<void> {
-    console.log(`Updating contract for issuer: ${iss}`);
+    const client = this.client();
+    console.log(`Adding oidc keys for: ${iss}`);
 
     const issHash = await this.getIssHash(iss);
-    const newKeys = await this.getNewKeys(issHash, keys);
+    const newKeys = await this.filterKeys(issHash, keys);
 
     if (newKeys.length === 0) {
       console.log("No new keys to add.");
@@ -40,63 +46,56 @@ export class ContractUpdater {
     }
 
     try {
-      const tx = await this.contract.addKeys(newKeys);
-      console.log(`Transaction sent: ${tx.hash}`);
-      await tx.wait();
+      const txHash = await client.writeContract({
+        abi: OidcKeyRegistryAbi,
+        functionName: "addKeys",
+        address: this.contractAddress,
+        args: [newKeys],
+      });
+      console.log(`Transaction sent: ${txHash}`);
+      await client.waitForTransactionReceipt({ hash: txHash });
       console.log("Transaction confirmed!");
     } catch (error) {
       console.error("Error updating contract:", error);
     }
   }
 
-  private async getNewKeys(issHash: string, keys: BaseKey[]): Promise<Key[]> {
-    const promises = keys.map((key) => {
-      const n = CircomBigInt.fromBase64(key.n).serialize();
+  private async filterKeys(issHash: Hex, keysFromProvider: BaseKey[]): Promise<Key[]> {
+    const client = this.client();
 
-      return this.contract.getKey(issHash, key.kid).then(
-        () => null,
-        () => ({
-          ...key,
-          n,
-          issHash,
-        }),
+    const rawContractKeys = await client.readContract({
+      functionName: "getKeys",
+      abi: OidcKeyRegistryAbi,
+      address: this.contractAddress,
+      args: [issHash],
+    });
+
+    const filteredKeys = keysFromProvider
+      .filter((k) =>
+        rawContractKeys.every((ck) => ck.kid !== k.kid),
       );
-    },
-    );
 
-    const results = await Promise.all(promises);
-    return results.filter((key): key is Key => key !== null);
+    return filteredKeys.map((bk) => keySchema.parse({
+      issHash: issHash,
+      kid: bk.kid,
+      rsaModulus: CircomBigInt.fromBase64(bk.n).serialize()
+        .map((bn) => BigInt(bn)),
+    }));
   }
 
-  private getNetwork(): types.Network {
-    if (!env.NETWORK) {
-      throw new Error("NETWORK is not set in config");
-    }
-
-    const networkMap: Record<string, types.Network> = {
-      mainnet: types.Network.Mainnet,
-      sepolia: types.Network.Sepolia,
-      localhost: types.Network.Localhost,
-    };
-
-    const network = networkMap[env.NETWORK.toLowerCase()];
-
-    if (!network) {
-      throw new Error(`Unknown or unsupported network: ${env.NETWORK}`);
-    }
-
-    return network;
-  }
-
-  private async getIssHash(iss: string): Promise<string> {
-    let issHash = this.issHashes.get(iss);
-    if (!issHash) {
-      issHash = await this.contract.hashIssuer.staticCall(iss);
-      if (!issHash) {
-        throw new Error("Failed to get issuer hash");
-      }
+  private async getIssHash(iss: string): Promise<Hex> {
+    const storedIssHash = this.issHashes.get(iss);
+    if (!storedIssHash) {
+      const client = this.client();
+      const issHash = await client.readContract({
+        address: this.contractAddress,
+        abi: OidcKeyRegistryAbi,
+        functionName: "hashIssuer",
+        args: [iss],
+      });
       this.issHashes.set(iss, issHash);
+      return issHash;
     }
-    return issHash;
+    return storedIssHash;
   }
 }
