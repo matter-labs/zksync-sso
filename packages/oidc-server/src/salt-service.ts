50e232cb82ffac382e884bd509344c82fd4d1c6a
calvo.generico <miguelfeliped@gmail.com>
2025-05-06 10:06:34 -0300

    fix: typo

--- packages/oidc-server/src/salt-service.ts
+++ packages/oidc-server/src/salt-service.ts
@@ -31,7 +31,7 @@ const JwtPayloadSchema = z.object({
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // 100 requets per window.
+  limit: 100, // 100 requests per window.
   standardHeaders: "draft-8",
   legacyHeaders: false,
 });
