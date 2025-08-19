56946c02460802b3788997d90af075f06db1e80b
Jack Hamer <jackhamer0990@gmail.com>
2025-08-19 13:44:53 +0300

    fix: reset contracts to main

--- packages/sdk/src/abi/index.ts
+++ packages/sdk/src/abi/index.ts
@@ -1,5 +1,6 @@
 export { AAFactoryAbi } from "./AAFactory.js";
 export { GuardianRecoveryValidatorAbi } from "./GuardianRecoveryValidator.js";
+export { OidcKeyRegistryAbi } from "./OidcKeyRegistry.js";
 export { OidcRecoveryValidatorAbi } from "./OidcRecoveryValidator.js";
 export { SessionKeyValidatorAbi } from "./SessionKeyValidator.js";
 export { SsoAccountAbi } from "./SsoAccount.js";
