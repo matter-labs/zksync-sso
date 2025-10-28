ac80037d35a8ee0d461317eaf2a1edd906ccffdb
jackpooleyml <186731786+jackpooleyml@users.noreply.github.com>
2025-10-28 14:53:27 +0100

    chore: cleanup unused code

--- packages/sdk-platforms/rust/zksync-sso-erc4337/crates/zksync-sso-erc4337-core/src/erc4337/signer.rs
+++ packages/sdk-platforms/rust/zksync-sso-erc4337/crates/zksync-sso-erc4337-core/src/erc4337/signer.rs
@@ -1,12 +1,4 @@
-use crate::erc4337::{
-    entry_point::PackedUserOperation,
-    user_operation::wrapper_v07::PackedUserOperationWrapperV07,
-};
-use alloy::{
-    hex,
-    primitives::{Address, Bytes, FixedBytes},
-    signers::{SignerSync, local::PrivateKeySigner},
-};
+use alloy::primitives::{Bytes, FixedBytes};
 use eyre;
 use std::sync::Arc;
 
@@ -17,33 +9,4 @@ pub type SignatureProvider =
 pub struct Signer {
     pub stub_signature: Bytes,
     pub provider: SignatureProvider,
-}
-
-pub fn sign_user_operation_v07_with_ecdsa(
-    uo: &PackedUserOperation,
-    ep: &Address,
-    chain_id: u64,
-    signer: PrivateKeySigner,
-) -> eyre::Result<PackedUserOperation> {
-    let uo_wrapper = PackedUserOperationWrapperV07::from(uo.clone());
-    let hash = uo_wrapper.hash(ep, chain_id);
-
-    println!("hash: {:?}", hash.clone());
-
-    let message = hash.0;
-
-    println!("message: {:?}", message.clone());
-
-    let message_bytes = message.to_vec();
-
-    println!("message_bytes: {:?}", message_bytes.clone());
-
-    let signature = signer.sign_message_sync(&message_bytes)?;
-    println!("signature: {:?}", signature);
-    let sig_vec: Vec<u8> = signature.into();
-    println!("hex::encode(sig_vec): {:?}", hex::encode(sig_vec.clone()));
-
-    let mut user_operation = uo.clone();
-    user_operation.signature = sig_vec.into();
-    Ok(user_operation)
 }
