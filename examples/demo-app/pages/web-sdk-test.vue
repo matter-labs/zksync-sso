bafa6a94cb47f3d14d69ee8dcd8f5549eb12bbe0
cbe <cbe@matterlabs.dev>
2025-11-03 00:33:37 -0800

    fix: deployment conflict
    Passkey signature fails?

--- examples/demo-app/pages/web-sdk-test.vue
+++ examples/demo-app/pages/web-sdk-test.vue
@@ -328,6 +328,27 @@ const passkeyConfig = ref({
   validatorAddress: "",
 });
 
+// Get funding account from query parameter (for test isolation)
+const route = useRoute();
+const fundingAccountIndex = computed(() => {
+  const param = route.query.fundingAccount;
+  return param ? parseInt(param, 10) : 0;
+});
+
+// Anvil private keys for accounts 0-9
+const anvilPrivateKeys = [
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", // Account #0
+  "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", // Account #1
+  "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a", // Account #2
+  "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6", // Account #3
+  "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a", // Account #4
+  "0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba", // Account #5
+  "0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e", // Account #6
+  "0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356", // Account #7
+  "0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97", // Account #8
+  "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6", // Account #9
+];
+
 // Computed property to check if all address params are valid
 const isAddressParamsValid = computed(() => {
   const params = addressParams.value;
@@ -446,11 +467,16 @@ async function deployAccount() {
     const eoaSignersAddresses = [eoaSignerAddress];
 
     // Use the appropriate private key based on the network
-    // Standard Anvil (port 8545): 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-    const deployerPrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // Anvil default account #0
+    // Use the funding account from query parameter (default to account #0)
+    const deployerPrivateKey = anvilPrivateKeys[fundingAccountIndex.value];
+    if (!deployerPrivateKey) {
+      throw new Error(`Invalid funding account index: ${fundingAccountIndex.value}`);
+    }
 
     // eslint-disable-next-line no-console
     console.log("Deploying account...");
+    // eslint-disable-next-line no-console
+    console.log("  Using Anvil account #" + fundingAccountIndex.value + " as deployer");
     // eslint-disable-next-line no-console
     console.log("  RPC URL:", rpcUrl);
     // eslint-disable-next-line no-console
