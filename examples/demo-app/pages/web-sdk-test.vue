127e6cb597200264087492a34910d0097a88de77
cbe <cbe@matterlabs.dev>
2025-11-03 00:18:57 -0800

    Revert "fix: flaky tests"
    This reverts commit 7319a526c63ab794ccb8f9339adaec9e08be1720.
    This was causing the e2e tests to fail?
    Very confused on this one

--- examples/demo-app/pages/web-sdk-test.vue
+++ examples/demo-app/pages/web-sdk-test.vue
@@ -441,47 +441,13 @@ async function deployAccount() {
     }
 
     // Add a rich Anvil wallet as an EOA signer for additional security
-    // Select which Anvil account to use based on URL parameter
-    // This allows parallel tests to use different accounts to avoid nonce conflicts
-    const urlParams = new URLSearchParams(window.location.search);
-    const accountIndex = parseInt(urlParams.get("fundingAccount") || "0", 10);
-
-    // Anvil test accounts (first 10) - private keys
-    const anvilPrivateKeys = [
-      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", // #0
-      "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", // #1
-      "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a", // #2
-      "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6", // #3
-      "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a", // #4
-      "0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba", // #5
-      "0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e", // #6
-      "0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356", // #7
-      "0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97", // #8
-      "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6", // #9
-    ];
-
-    // Corresponding public addresses
-    const anvilAddresses = [
-      "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", // #0
-      "0x70997970C51812dc3A010C7d01b50e0d17dc79C8", // #1
-      "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", // #2
-      "0x90F79bf6EB2c4f870365E785982E1f101E93b906", // #3
-      "0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65", // #4
-      "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc", // #5
-      "0x976EA74026E726554dB657fA54763abd0C3a0aa9", // #6
-      "0x14dC79964da2C08b23698B3D3cc7Ca32193d9955", // #7
-      "0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f", // #8
-      "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720", // #9
-    ];
-
-    const eoaSignerAddress = anvilAddresses[accountIndex] || anvilAddresses[0];
+    // Using Anvil account #1 (0x70997970C51812dc3A010C7d01b50e0d17dc79C8)
+    const eoaSignerAddress = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
     const eoaSignersAddresses = [eoaSignerAddress];
 
     // Use the appropriate private key based on the network
-    const deployerPrivateKey = anvilPrivateKeys[accountIndex] || anvilPrivateKeys[0];
-
-    // eslint-disable-next-line no-console
-    console.log("  Using Anvil account #" + accountIndex + " for deployment");
+    // Standard Anvil (port 8545): 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+    const deployerPrivateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // Anvil default account #0
 
     // eslint-disable-next-line no-console
     console.log("Deploying account...");
@@ -707,31 +673,9 @@ async function fundSmartAccount() {
     // Create provider
     const provider = new ethers.JsonRpcProvider(rpcUrl);
 
-    // Select which Anvil account to use for funding based on URL parameter
-    // This allows parallel tests to use different accounts to avoid nonce conflicts
-    // Default to account #1, but tests can specify ?fundingAccount=2 to use account #2
-    const urlParams = new URLSearchParams(window.location.search);
-    const fundingAccountIndex = parseInt(urlParams.get("fundingAccount") || "1", 10);
-
-    // Anvil test accounts (first 10)
-    const anvilAccounts = [
-      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", // #0
-      "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", // #1
-      "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a", // #2
-      "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6", // #3
-      "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a", // #4
-      "0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba", // #5
-      "0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e", // #6
-      "0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356", // #7
-      "0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97", // #8
-      "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6", // #9
-    ];
-
-    const eoaSignerPrivateKey = anvilAccounts[fundingAccountIndex] || anvilAccounts[1];
+    // EOA signer private key (Anvil account #1)
+    const eoaSignerPrivateKey = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
     const eoaSigner = new ethers.Wallet(eoaSignerPrivateKey, provider);
-
-    // eslint-disable-next-line no-console
-    console.log("  Using Anvil account #" + fundingAccountIndex + " for funding");
 
     // Convert amount to wei
     const amountWei = ethers.parseEther(fundParams.value.amount);
