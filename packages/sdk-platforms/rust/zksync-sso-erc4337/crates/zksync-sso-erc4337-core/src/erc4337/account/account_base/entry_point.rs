87da646c626abf62daf9649c782d49d727f9e11d
cbe <cbe@matterlabs.dev>
2025-11-05 08:21:13 -0800

    fix: update to contracts on main
    merged bundler

--- packages/sdk-platforms/rust/zksync-sso-erc4337/crates/zksync-sso-erc4337-core/src/erc4337/account/account_base/entry_point.rs
+++ packages/sdk-platforms/rust/zksync-sso-erc4337/crates/zksync-sso-erc4337-core/src/erc4337/account/account_base/entry_point.rs
@@ -6,6 +6,6 @@ pub async fn get_entry_point_address<P: Provider + Send + Sync + Clone>(
     provider: P,
 ) -> eyre::Result<Address> {
     let account_instance = AccountBase::new(account_address, provider.clone());
-    let entry_point_address = account_instance.ENTRY_POINT().call().await?;
+    let entry_point_address = account_instance.ENTRY_POINT_V08().call().await?;
     Ok(entry_point_address)
 }
