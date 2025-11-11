af27407cf0488910e9ebf8891daaa45681dab351
cbe <cbe@matterlabs.dev>
2025-11-11 11:33:46 -0800

    feat: add first phase
    Starting fresh for signatures

--- packages/sdk-platforms/rust/zksync-sso-erc4337/crates/zksync-sso-erc4337-core/src/erc4337/account/modular_smart_account/session.rs
+++ packages/sdk-platforms/rust/zksync-sso-erc4337/crates/zksync-sso-erc4337-core/src/erc4337/account/modular_smart_account/session.rs
@@ -1,11 +1,13 @@
 pub mod active;
 pub mod create;
+pub mod encode;
 pub mod hash;
 pub mod period_id;
 pub mod revoke;
 pub mod send;
 pub mod session_lib;
 pub mod signature;
+pub mod signature_wasm;
 pub mod status;
 
 use alloy::sol;
