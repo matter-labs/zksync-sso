9c31ecb9e46a1bd9db993024ec5bb5f52d3205f3
Jack Hamer <47187316+JackHamer09@users.noreply.github.com>
2025-11-14 21:08:28 +0200

    feat: 4337 smart account viem client wrappers (#225)

--- packages/sdk-4337/src/client/passkey/index.ts
+++ packages/sdk-4337/src/client/passkey/index.ts
@@ -1,5 +1,14 @@
 export { toPasskeySmartAccount, type ToPasskeySmartAccountParams } from "./account.js";
 export {
+  createPasskeyClient,
+  type CreatePasskeyClientParams,
+  type PasskeyClient,
+} from "./client.js";
+export {
+  type PasskeyClientActions,
+  passkeyClientActions,
+} from "./client-actions.js";
+export {
   type CreateCredentialOptions,
   createWebAuthnCredential,
   signWithPasskey,
