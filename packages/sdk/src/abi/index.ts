c98e9ec18d76c0cd807051ffe7b196b8e30b9126
cbe <cbe@matterlabs.dev>
2025-12-23 22:53:21 -0800

    fix: add oidc abis
    This reverts commit d58b659b1b68f9bc13d969c8190d886f8a2fbb15.
    adds abis

--- packages/sdk/src/abi/index.ts
+++ packages/sdk/src/abi/index.ts
@@ -1,5 +1,7 @@
 export { AAFactoryAbi } from "./AAFactory.js";
 export { GuardianRecoveryValidatorAbi } from "./GuardianRecoveryValidator.js";
+export { OidcKeyRegistryAbi } from "./OidcKeyRegistry.js";
+export { OidcRecoveryValidatorAbi } from "./OidcRecoveryValidator.js";
 export { SessionKeyValidatorAbi } from "./SessionKeyValidator.js";
 export { SsoAccountAbi } from "./SsoAccount.js";
 export { WebAuthValidatorAbi } from "./WebAuthValidator.js";
