716fbcbf2527668657f4cd1c27952d22d8b8e259
cbe <cbe@matterlabs.dev>
2026-01-17 23:31:21 -0800

    fix: more clippy warnings

--- packages/sdk-platforms/rust/zksync-sso-erc4337/crates/zksync-sso-erc4337-ffi-web/src/lib.rs
+++ packages/sdk-platforms/rust/zksync-sso-erc4337/crates/zksync-sso-erc4337-ffi-web/src/lib.rs
@@ -2977,6 +2977,9 @@ impl Client {
     }
 }
 
+// Re-export session functions from account module
+pub use account::modular_smart_account::session::list::get_active_sessions_wasm;
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -3336,6 +3339,3 @@ mod tests {
         }
     }
 }
-
-// Re-export session functions from account module
-pub use account::modular_smart_account::session::list::get_active_sessions_wasm;
